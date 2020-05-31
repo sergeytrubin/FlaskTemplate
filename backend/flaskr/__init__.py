7372d2c36a7bb508b51e931698a9a187745cf114
sergeytrubin <strubin@gmail.com>
2020-05-31 17:11:03 +0300

    Added get_plants()

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,9 +1,10 @@
 import os
 from flask import (
     Flask,
-    jsonify
+    jsonify,
+    request
 )
-from models import setup_db, Pland
+from .models import setup_db, Plant
 from flask_cors import CORS
 
 def create_app(test_config=None):
@@ -27,8 +28,25 @@ def create_app(test_config=None):
         response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
         return response
 
-    @app.route('/')
-    def hello():
-        return jsonify({'message': 'HELLO WORLD'})
+
+    @app.route('/plants')
+    def get_plants():
+        '''
+        The "page =..." says:
+        look at the arguments of request object and get the value of key 'page'. 
+        If key 'page' does not exists assign the default value which is 1 as specifified below. The we specify the type to integer.
+        '''
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * 10 # Starting index - start with page '0', because the first page gets the default value of 1.
+        end = start + 10 # Ending index - show 10 results per page
+        plants = Plant.query.all()
+        formatted_plants = [plant.format() for plant in plants]
+
+        return jsonify({
+            'success':True,
+            'plants': formatted_plants[start:end], # Show results according to starting and enging indexes
+            'total_plants': len(formatted_plants) # show total number of records
+        })
+
 
     return app
